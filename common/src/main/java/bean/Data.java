4dc6c8b76698200b3fd64c375b7468b6c33cda4e
goatdidi <631298635@qq.com>
2021-01-15 19:14:14 +0800

    first commit

---
+++ common/src/main/java/bean/Data.java
@@ -0,0 +1,10 @@
+package bean;
+
+/***
+ * @author goatdidi
+ **/
+public abstract class Data implements value{
+    protected String content;
+
+
+}
---
+++ common/src/main/java/bean/value.java
@@ -0,0 +1,9 @@
+package bean;
+
+/***  值生成对象接口
+ * @author goatdidi
+ **/
+public interface value {
+    Object getValue();
+    void setValue(Object val);
+}
